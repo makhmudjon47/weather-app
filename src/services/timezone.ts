4878295f6a5b77bc83ca9a765f70d2214fe3247f
Makhmudjon <maxmudinadullayev98@gmail.com>
2021-06-14 20:07:53 +0500

    Services updated to use SSL

--- src/services/timezone.ts
+++ src/services/timezone.ts
@@ -4,6 +4,6 @@ const apiKey = process.env.API_KEY_TIMEZONE
 
 export default {
     getCurrentTime({ lat, lng }: { lat: number, lng: number }): Promise<AxiosResponse<any>> {
-        return axios.get(`http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lng}`)
+        return axios.get(`https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lng}`)
     },
 }
--- src/services/weather.ts
+++ src/services/weather.ts
@@ -4,9 +4,9 @@ const apiKey = process.env.API_KEY_OPENWEATHER
 
 export default {
     getCurrentWeather(lat: number, lng: number): Promise<AxiosResponse<any>> {
-        return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`)
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`)
     },
     getForecastWeather(lat: number, lng: number): Promise<AxiosResponse<any>> {
-        return axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`)
+        return axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`)
     }
 }
