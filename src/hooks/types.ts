05a5e49e0bcfc6d5282e50d1b47f860926304bfa
Makhmudjon <maxmudinadullayev98@gmail.com>
2021-06-15 16:05:14 +0500

    Global hooks and utils created

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,2 +1,25 @@
-export interface IPosition { lat: number, lng: number }
-export interface ISuggestion { text: string, placeId: string, matched_substrings: [{ length: number, offset: number }] }
+export interface IPosition {
+    lat: number,
+    lng: number
+}
+export interface ISuggestion {
+    text: string,
+    placeId: string,
+    matched_substrings: [
+        {
+            length: number,
+            offset: number
+        }
+    ]
+}
+export interface IForecast {
+    date: Date,
+    min_temp: number,
+    max_temp: number,
+    weekString: string,
+    dateString: string,
+    status: {
+        desc: string
+        icon: string
+    }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,14 @@
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "November", "Dec"];
+
+export function getMonthName(date: Date): string {
+    return monthNames[date.getMonth()]
+}
+
+export function getWeekName(date: Date): string {
+    return dayNames[date.getDay()]
+}
+
 export function getWeatherIcon(code: string, quality?: string) {
     return `http://openweathermap.org/img/wn/${code}@${quality || '2x'}.png`
 }
