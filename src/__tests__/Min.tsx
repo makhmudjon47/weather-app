7da1b6cee588ca8a16256ecc40bc496b52d729c1
Makhmudjon <maxmudinadullayev98@gmail.com>
2021-06-17 15:17:04 +0500

    little update

--- src/__tests__/Min.tsx
+++ src/__tests__/Min.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer';
-import Max from '../components/main/Info/Max';
+import Max from '../components/main/Info/Mini';
 
 test('InfoPanel test created', () => {
   const component = renderer.create(<Max temp="temp" city="city" desc="desc" icon="icon" time="time" />).toJSON();
